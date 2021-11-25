a6b4067270417e61c34ad65c3e397c482fda2816
davi <davi_berto1@hotmail.com.br>
2021-11-25 17:31:30 -0300

    toString() classe Contrato

--- Contrato.java
+++ Contrato.java
@@ -1,17 +1,16 @@
-
-
 public class Contrato {
+
 	private Veiculo veiculo;
-	private int dias; 
+	private int dias;
 	private double kmInicial;
 	private double kmFinal;
-	
+
 	public Contrato(Veiculo veiculo, int dias, double kmInicial, double kmFinal) {
 		this.veiculo = veiculo;
 		this.dias = dias;
 		this.kmInicial = kmInicial;
 		this.kmFinal = kmFinal;
-	}	
+	}
 
 	public Veiculo getVeiculo() {
 		return veiculo;
@@ -28,7 +27,7 @@ public class Contrato {
 	public void setDias(int dias) {
 		this.dias = dias;
 	}
-	
+
 	public double getKmInicial() {
 		return kmInicial;
 	}
@@ -46,18 +45,19 @@ public class Contrato {
 	}
 
 	public double valorKM() {
-		return (kmFinal- kmInicial) * veiculo.getTaxaKM();
+		return (kmFinal - kmInicial) * veiculo.getTaxaKM();
 	}
-	
+
 	public double custoDias() {
 		return dias * veiculo.getTaxaDia();
 	}
-	
+
 	public double custoTotal() {
 		return custoDias() + veiculo.getTaxaVeiculo() + valorKM();
 	}
-	
+
 	public String toString() {
-		return String.format("Valor total a ser pago: %.2f%n", custoTotal());
-	}	
+		return String.format("Dias alugados: %d%nQuilometros rodados: %.2f%nValor total a ser pago: %.2f%n", dias,
+				(kmFinal - kmInicial), custoTotal());
+	}
 }
